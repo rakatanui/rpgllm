aa2fe309e4ef091429e959545c05f3fdfb6537b0
Andrei Krauchanka <grafgarrsky@gmail.com>
2026-09-19 14:38:36 +0300

    fix: match current appearance route before image route (#34)

--- rpg/urls.py
+++ rpg/urls.py
@@ -17,14 +17,14 @@ urlpatterns = [
         name="human_player_fragment",
     ),
     path(
-        "scene/<int:scene_id>/human/<int:player_id>/appearance/<int:appearance_id>/<str:image_kind>/",
-        views.human_appearance_image,
-        name="human_appearance_image",
-    ),
-    path(
         "scene/<int:scene_id>/human/<int:player_id>/appearance/<int:appearance_id>/current/",
         views.set_human_current_appearance,
         name="set_human_current_appearance",
+    ),
+    path(
+        "scene/<int:scene_id>/human/<int:player_id>/appearance/<int:appearance_id>/<str:image_kind>/",
+        views.human_appearance_image,
+        name="human_appearance_image",
     ),
     path(
         "scene/<int:scene_id>/human/<int:player_id>/character-image/",
