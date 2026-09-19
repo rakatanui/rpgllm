050ceeb0e796a3900a4777206cd0adfaca1ee982
Andrei Krauchanka <grafgarrsky@gmail.com>
2026-09-19 13:40:52 +0300

    config: add persistent media root for character portraits

--- mraz/settings.py
+++ mraz/settings.py
@@ -112,6 +112,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "rpg" / "static"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
